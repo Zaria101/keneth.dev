f0edd1fd9cdf55b9461c83e4a0ed92715ace4fad
Zaria101 <alexmallorca12@gmail.com>
2022-03-12 16:03:05 +0100

    Certificate edited

--- main.js
+++ main.js
@@ -89,3 +89,23 @@ toggle.onclick = function () {
 }
 
 
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+ // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
